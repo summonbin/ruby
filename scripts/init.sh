33f8c309f79eb9803482b26a66cde074e1c5bc1f
Jason Nam <contact@jasonnam.com>
2018-10-02 21:38:08 +0700

    Use double quotation marks

--- scripts/init.sh
+++ scripts/init.sh
@@ -17,7 +17,7 @@ DEFAULT_CACHE_PATH=$3
 #### Build driver ####
 ######################
 
-mkdir -p $INSTALL_PATH/$DRIVER_NAME
+mkdir -p "$INSTALL_PATH/$DRIVER_NAME"
 curl -L "$BASE_URL/$VERSION/scripts/setup.sh" -o "$INSTALL_PATH/$DRIVER_NAME/setup.sh"
 curl -L "$BASE_URL/$VERSION/scripts/ruby.sh" -o "$INSTALL_PATH/$DRIVER_NAME/ruby.sh"
 curl -L "$BASE_URL/$VERSION/scripts/run.sh" -o "$INSTALL_PATH/$DRIVER_NAME/run.sh"
@@ -27,7 +27,7 @@ curl -L "$BASE_URL/$VERSION/scripts/run.sh" -o "$INSTALL_PATH/$DRIVER_NAME/run.s
 #### Build scheme ####
 ######################
 
-mkdir -p $SCHEME_PATH/$DRIVER_NAME/rbenv
+mkdir -p "$SCHEME_PATH/$DRIVER_NAME/rbenv"
 if [ ! -f "$SCHEME_PATH/$DRIVER_NAME/rbenv/cache" ]
 then
   echo "$DEFAULT_CACHE_PATH/$DRIVER_NAME/rbenv" > "$SCHEME_PATH/$DRIVER_NAME/rbenv/cache"
@@ -37,7 +37,7 @@ then
   echo "v1.1.1" > "$SCHEME_PATH/$DRIVER_NAME/rbenv/version"
 fi
 
-mkdir -p $SCHEME_PATH/$DRIVER_NAME/bundler
+mkdir -p "$SCHEME_PATH/$DRIVER_NAME/bundler"
 if [ ! -f "$SCHEME_PATH/$DRIVER_NAME/bundler/cache" ]
 then
   echo "$DEFAULT_CACHE_PATH/$DRIVER_NAME/bundler" > "$SCHEME_PATH/$DRIVER_NAME/bundler/cache"
@@ -47,7 +47,7 @@ then
   echo "v1.16.2" > "$SCHEME_PATH/$DRIVER_NAME/bundler/version"
 fi
 
-mkdir -p $SCHEME_PATH/$DRIVER_NAME/gem
+mkdir -p "$SCHEME_PATH/$DRIVER_NAME/gem"
 if [ ! -f "$SCHEME_PATH/$DRIVER_NAME/gem/cache" ]
 then
   echo "$DEFAULT_CACHE_PATH/$DRIVER_NAME/gem" > "$SCHEME_PATH/$DRIVER_NAME/gem/cache"
