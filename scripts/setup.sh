b9085f8b4b3f966116f44723b6ec9e6a00446a59
Jason Nam <contact@jasonnam.com>
2019-05-04 10:23:05 +0700

    Ignore setup ruby on system

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -26,53 +26,46 @@ BUNDLER_VERSION=$(<"$CONFIG_DIR/bundler/version")
 GEM_CACHE_DIR=$( eval "echo $(<"$CONFIG_DIR/gem/cache")" )
 
 
-##############################
-#### Check version system ####
-##############################
-
-if [ "$TARGET_RUBY_VERSION" = "system" ]
-then
-  return
-fi
-
-
 ####################
 #### Setup rbenv ###
 ####################
 
-RBENV_DIR="$RBENV_CACHE_DIR/$RBENV_VERSION"
-RBENV_BIN="$RBENV_DIR/bin"
-
-# Clone rbenv
-if [ ! -d "$RBENV_BIN" ]
+if [ ! "$TARGET_RUBY_VERSION" = "system" ]
 then
-  rm -rf "$RBENV_DIR"
-  git clone "$RBENV_REPO_URL" "$RBENV_DIR" -b "$RBENV_VERSION" --single-branch --depth 1
+  RBENV_DIR="$RBENV_CACHE_DIR/$RBENV_VERSION"
+  RBENV_BIN="$RBENV_DIR/bin"
+
+  # Clone rbenv
+  if [ ! -d "$RBENV_BIN" ]
+  then
+    rm -rf "$RBENV_DIR"
+    git clone "$RBENV_REPO_URL" "$RBENV_DIR" -b "$RBENV_VERSION" --single-branch --depth 1
+  fi
+
+  RUBY_BUILD_DIR="$RBENV_DIR/plugins/ruby-build"
+  RUBY_BUILD_BIN="$RUBY_BUILD_DIR/bin"
+
+  # Clone ruby-build
+  if [ ! -d "$RUBY_BUILD_BIN" ]
+  then
+    rm -rf "$RUBY_BUILD_DIR"
+    git clone "$RUBY_BUILD_REPO_URL" "$RUBY_BUILD_DIR" --single-branch --depth 1
+  fi
+
+  # Setup env
+  export PATH="$RBENV_BIN:$PATH"
+  export PATH="$RBENV_BIN/rbenv/shims:$PATH"
+  export PATH="$RBENV_DIR/plugins/ruby-build/bin:$PATH"
+  export RBENV_ROOT="$RBENV_DIR"
+  eval "$(rbenv init -)"
+
+  # Set Ruby version
+  if ! [ -d "$RBENV_DIR/versions/$TARGET_RUBY_VERSION" ]
+  then
+    rbenv install "$TARGET_RUBY_VERSION"
+  fi
+  export RBENV_VERSION="$TARGET_RUBY_VERSION"
 fi
-
-RUBY_BUILD_DIR="$RBENV_DIR/plugins/ruby-build"
-RUBY_BUILD_BIN="$RUBY_BUILD_DIR/bin"
-
-# Clone ruby-build
-if [ ! -d "$RUBY_BUILD_BIN" ]
-then
-  rm -rf "$RUBY_BUILD_DIR"
-  git clone "$RUBY_BUILD_REPO_URL" "$RUBY_BUILD_DIR" --single-branch --depth 1
-fi
-
-# Setup env
-export PATH="$RBENV_BIN:$PATH"
-export PATH="$RBENV_BIN/rbenv/shims:$PATH"
-export PATH="$RBENV_DIR/plugins/ruby-build/bin:$PATH"
-export RBENV_ROOT="$RBENV_DIR"
-eval "$(rbenv init -)"
-
-# Set Ruby version
-if ! [ -d "$RBENV_DIR/versions/$TARGET_RUBY_VERSION" ]
-then
-  rbenv install "$TARGET_RUBY_VERSION"
-fi
-export RBENV_VERSION="$TARGET_RUBY_VERSION"
 
 
 #######################
